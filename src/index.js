d6e5652dbe4065fbf8184646f6d3eb9599afc10b
Joseph Lee <jhjlee702@gmail.com>
2025-02-13 19:52:55 -0700

    finally figured out the Tailwind stuff lol

--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "tailwindcss/base";
-import "tailwindcss/components";
-import "tailwindcss/utilities";
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
