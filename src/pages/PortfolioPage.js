b229f48e1c121738d8a9abf0b068acd398d5dc4e
Joseph Lee <jhjlee702@gmail.com>
2025-02-17 17:46:52 -0700

    remove Tailwind CSS class

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -91,7 +91,7 @@ const PortfolioPage = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
+    <div>
       <h1 className="text-3xl font-bold text-gray-800">
         {portfolio ? portfolio.name : "Portfolio does not exist."}
       </h1>
