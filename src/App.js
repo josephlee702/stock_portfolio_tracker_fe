f342a975e0ed300e71b7f013198607fdfdaaad37
Joseph Lee <jhjlee702@gmail.com>
2025-02-17 17:06:20 -0700

    import NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PortfolioPage from "./pages/PortfolioPage";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   return (
     <Router>
+      <NavBar /> 
+      <main className = "p-4">
         <Routes>
           <Route path="/" element={<h1>Welcome to the Stock Portfolio Tracker!</h1>} />
           <Route path="/portfolios/:id" element={<PortfolioPage />} />
         </Routes>
+      </main>
     </Router>
   );
 };
