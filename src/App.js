32a77f020e2bd9fa066ae5bfed0b3766998c4bee
Joseph Lee <jhjlee702@gmail.com>
2025-02-20 18:31:39 -0700

    add import HomePage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PortfolioPage from "./pages/PortfolioPage";
+import HomePage from "./pages/HomePage";
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -10,7 +11,7 @@ const App = () => {
       <NavBar /> 
       <main className = "p-4">
         <Routes>
-          <Route path="/" element={<h1>Welcome to the Stock Portfolio Tracker!</h1>} />
+          <Route path="/" element={<HomePage/>} />
           <Route path="/portfolios/:id" element={<PortfolioPage />} />
         </Routes>
       </main>
