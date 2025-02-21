cb38652937251cd92f9128c498178639dfbcb614
Joseph Lee <jhjlee702@gmail.com>
2025-02-20 18:31:55 -0700

    display portfolios on Home Page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import api from "../services/api"; 
+import { Link } from "react-router-dom";
+
+const HomePage = () => {
+  const [portfolios, setPortfolios] = useState([]);
+  const token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo2fQ.-XdcSMywrlroe_kS3juSFq7T1vD3c14XhaOgkQrCPMY";
+
+  useEffect(() => {
+    const fetchPortfolios = async () => {
+      try {
+        const response = await api.get(`/portfolios`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        setPortfolios(response.data);
+      } catch (error) {
+        console.error("Error fetching portfolios:", error);
+      }
+    };
+    fetchPortfolios();
+  }, [token]);
+
+  return (
+    <div className="container mt-4">
+      <h1 className="mb-3">Your Portfolios</h1>
+      {portfolios.length > 0 ? (
+        <ul className="list-group">
+          {portfolios.map((portfolio) => (
+            <li key={portfolio.id} className="list-group-item">
+              <h2><Link to={`/portfolios/${portfolio.id}`}>{portfolio.name}</Link></h2>
+              <p>Current Market Value: </p>
+              <p>Created on: {new Date(portfolio.created_at).toLocaleDateString()}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No portfolios found.</p>
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
