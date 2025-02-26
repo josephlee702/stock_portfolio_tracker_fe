8c3edd677f379aee8d1ddd919d7997ff0d341276
Joseph Lee <jhjlee702@gmail.com>
2025-02-26 16:38:19 -0700

    set up route for Login and Register and adjust CSS styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,32 @@ import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = ({ darkMode, setDarkMode }) => {
+  const token = localStorage.getItem("token");
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  }
  
   return (
-    <nav className="navbar">
-      <div className="nav-links">
-        <Link to="/" className="ms-3">Home</Link>
-        <Link to="/portfolio">Portfolio</Link>
-        <Link to="/settings">Settings</Link>
-      </div>
-      
-      <button className="dark-mode-toggle" onClick={() => setDarkMode(!darkMode)}>
+    <nav className="navbar d-flex flex-row justify-content-start p-2 bg-dark text-white">
+      <Link to="/">Home</Link>
+
+      {token ? (
+        <button onClick={handleLogout} className="btn btn-danger">
+          Logout
+        </button>
+      ) : (
+        <>
+          <Link to="/login"> Log In </Link>
+          <Link to="/register">Register</Link>
+        </>
+      )}
+      <div className="ms-auto">
+        <button onClick={() => setDarkMode(!darkMode)} className="btn btn-primary">
         {darkMode ? "☀" : "☾"}
-      </button>
+        </button>
+      </div>
     </nav>
   );
 };
