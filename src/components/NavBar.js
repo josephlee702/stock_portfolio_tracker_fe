ac1c61098760075305bbbca3e75257a6e6c0b6a0
Joseph Lee <jhjlee702@gmail.com>
2025-03-03 20:46:55 -0700

    take out useState and include fetchUserData in useEffect array

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/Authcontext";
 import "./NavBar.css";
@@ -8,7 +8,7 @@ const NavBar = ({ darkMode, setDarkMode }) => {
 
   useEffect(() => {
     fetchUserData();
-  }, []);
+  }, [fetchUserData]);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
