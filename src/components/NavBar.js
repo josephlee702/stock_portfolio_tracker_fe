302e0549dda60e2e843c43f3d246b5ac9f7437a6
Joseph Lee <jhjlee702@gmail.com>
2025-03-03 20:03:41 -0700

    import more stuff and change log in to log out when user is detected

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/Authcontext";
 import "./NavBar.css";
 
 const NavBar = ({ darkMode, setDarkMode }) => {
-  const token = localStorage.getItem("token");
+  const { user, fetchUserData } = useContext(AuthContext);
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -14,16 +19,20 @@ const NavBar = ({ darkMode, setDarkMode }) => {
     <nav className="navbar d-flex flex-row justify-content-start p-2 bg-dark text-white">
       <Link to="/">Home</Link>
 
-      {token ? (
-        <button onClick={handleLogout} className="btn btn-primary rounded-pill">
-          Logout
-        </button>
+      {user ? (
+        <>
+          <span className="ms-2">Welcome, {user.name}</span>
+          <button onClick={handleLogout} className="btn btn-primary rounded-pill ms-2">
+            Logout
+          </button>
+        </>
       ) : (
         <>
-          <Link to="/login"> Log In </Link>
-          <Link to="/signup">Sign Up</Link>
+          <Link to="/login" className="ms-2">Log In</Link>
+          <Link to="/signup" className="ms-2">Sign Up</Link>
         </>
       )}
+
       <div className="ms-auto">
         <button onClick={() => setDarkMode(!darkMode)} className="btn btn-primary">
         {darkMode ? "☀" : "☾"}
